dc2e8823d60c3adde8e0358b7daceb021f859914
Nithin257 <nithinnithinkn@gmail.com>
2023-02-06 18:34:47 +0530

    pushing the code

---
+++ Triangle3.java
@@ -0,0 +1,15 @@
+class Triangle3
+{
+static double area ( )
+{
+int b= 10;
+int h= 5;
+double result= 0.5*b*h;
+return result;
+}
+public static void main (String[] args)
+{
+double x= area();
+System.out.println(x);
+}
+}
